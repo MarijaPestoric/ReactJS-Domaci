adfc04310c4874ed60d6b4ca8f6c20f6ab3cd562
MarijaPestoric <60827834+MarijaPestoric@users.noreply.github.com>
2021-12-30 19:01:19 +0100

    Validation, notification
    Edited validation, added react-notification npm package

--- Zadatak1/registration-page/src/App.js
+++ Zadatak1/registration-page/src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import { NotificationContainer, NotificationManager } from 'react-notifications';
 class App extends React.Component {
   state = {
     users: [
-      {}
+      {
+        usersId: 1,
+        firstName: "Ana",
+        lastName: "Anic",
+        username: "anica123",
+        email: "anica@gmail.com",
+        password: "Anica!123"
+      }
     ],
     firstName: {
       name: "",
@@ -31,45 +39,49 @@ class App extends React.Component {
     submitted: false,
     usersId: 1
   }
-  //http://jsonblob.com/925468983633920000
+
   handleChange = (e) => {
     let name = e.target.name;
     this.setState({ [name]: e.target.value })
   }
-  
+
   handleSubmit = (e) => {
     e.preventDefault()
     let passwordRegex = new RegExp("(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})")
     let emailRegex = new RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2,3}');
+    let usernameRegex = /^.{6,12}$/;
     let { firstName, lastName, username, email, password, confirmPassword } = this.state;
-    if (firstName.name==="") {
-      console.log("Input field first name is empty!?");
-      this.setState({firstName: {isValid: "error"}})
-    }  if (lastName.name==="") {
-      console.log("Input field last name is empty!?");
-      this.setState({lastName: {isValid: "error"}})
-    }  if (username.name==="") {
-      console.log("Input field username is empty!");
-      this.setState({username: {isValid: "error"}})
-    }  if (email.name==="" || !emailRegex.test(email)) {
-      console.log("Not valid email address");
-      this.setState({email: {isValid: "error"}})
-    }  if (!passwordRegex.test(password)) {
-      console.log("Password is not strong enough!");
-      this.setState({password: {isValid: "error"}})
-    } if(password !== confirmPassword){
-      console.log("Passwords dont match!");
-      this.setState({confirmPassword: {isValid: "error"},
-      password: {isValid: "error"}})
-    } if (lastName.name!=="" && firstName.name!=="" && username.name!=="" && email.name!=="" && emailRegex.test(email) && passwordRegex.test(password) && password === confirmPassword) {
+    if (firstName.name === "") {
+      NotificationManager.warning('Input field first name is empty.', 'Please fill all inputs!', 5000);
+      this.setState({ firstName: { isValid: "error" } })
+    } if (lastName.name === "") {
+      NotificationManager.warning('Input field last name is empty.', 'Please fill all inputs!', 5000);
+      this.setState({ lastName: { isValid: "error" } })
+    } if (!usernameRegex.test(username)) {
+      NotificationManager.warning('Input field username should have minimum 6 and maximum 12 characters.', 'Please fill all inputs!', 5000);
+      this.setState({ username: { isValid: "error" } })
+    } if (email.name === "" || !emailRegex.test(email)) {
+      NotificationManager.warning('Invalid email address.', 'Please enter valid email address!', 5000);
+      this.setState({ email: { isValid: "error" } })
+    } if (!passwordRegex.test(password)) {
+      NotificationManager.warning('Password is not strong enough!', 'Your password should have at least 8 characters, of which at least one uppercase, one lowercase letter, number and special character should be included.', 5000);
+      this.setState({ password: { isValid: "error" } })
+    } if (password !== confirmPassword) {
+      NotificationManager.warning("Passwords you entered don't match", 'Please try again!', 5000);
+      this.setState({
+        confirmPassword: { isValid: "error" },
+        password: { isValid: "error" }
+      })
+    } if (lastName.name !== "" && firstName.name !== "" && username.name !== "" && email.name !== "" && emailRegex.test(email) && passwordRegex.test(password) && password === confirmPassword) {
       let newUser = {
         usersId: this.state.users.length + 1,
         firstName: this.state.firstName,
         lastName: this.state.lastName,
         username: this.state.username,
-        email: this.state.email
+        email: this.state.email,
+        password: this.state.password
       }
-     let users = this.state.users;
+      let users = this.state.users;
       users.push(newUser)
       this.setState({ users })
       fetch("https://jsonblob.com/api/jsonBlob/",
@@ -81,29 +93,34 @@ class App extends React.Component {
           },
           body: JSON.stringify(this.state.users)
         })
-        .then(response => response.json())
-        .then(data => console.log(data))
+        .then(response => {
+          let blobUrl = response.headers.get(
+            "Location"
+          );
+          NotificationManager.success('You have registered successfully.', 'Thank you!');
+          console.log(blobUrl);
+        })
         .catch(err => console.log("Error! " + err))
-        console.log("SUBMITTED");
+      console.log("SUBMITTED");
     } else {
       console.log("Not submitted!");
     }
   }
   render() {
-    const {firstName, lastName, username, email, password, confirmPassword} = this.state;
+    const { firstName, lastName, username, email, password, confirmPassword } = this.state;
     return (
       <div className="App">
         <div className="row main-container">
-        <h1>Create Account.</h1>
+          <h1>Create Account.</h1>
           <form method='POST' onSubmit={this.handleSubmit}>
-              <div className='row col s12'>
+            <div className='row col s12'>
               <div className="input-field col s12 l6">
                 <i className="material-icons prefix">person</i>
-                <input id="first_name" name='firstName' type="text" className={firstName.isValid}  onChange={this.handleChange} />
+                <input id="first_name" name='firstName' type="text" className={firstName.isValid} onChange={this.handleChange} />
                 <label htmlFor="first_name">First Name </label>
               </div>
               <div className="input-field col s12 l6">
-              <i className="material-icons prefix">person</i>
+                <i className="material-icons prefix">person</i>
                 <input id="last_name" name='lastName' type="text" className={lastName.isValid} onChange={this.handleChange} />
                 <label htmlFor="last_name">Last Name</label>
               </div>
@@ -117,22 +134,23 @@ class App extends React.Component {
                 <input id="email" name='email' type="text" className={email.isValid} onChange={this.handleChange} />
                 <label htmlFor="email">Email</label>
               </div>
-              <div className="input-field col s12 m6 l6">
+              <div className="input-field col s12 l6">
                 <i className="material-icons prefix">lock</i>
                 <input id="password" name='password' type="password" className={password.isValid} onChange={this.handleChange} />
                 <label htmlFor="password">Password</label>
               </div>
-              <div className="input-field col s12 m6 l6">
-              <i className="material-icons prefix">lock_outline</i>
+              <div className="input-field col s12 l6">
+                <i className="material-icons prefix">lock_outline</i>
                 <input id="confirm-password" name='confirmPassword' type="password" className={confirmPassword.isValid} onChange={this.handleChange} />
                 <label htmlFor="confirm-password">Confirm password</label>
               </div>
-              </div>
+            </div>
             <button className="btn waves-effect waves-light" type="submit" name="action">Submit
               <i className="material-icons right">send</i>
             </button>
           </form>
         </div>
+        <NotificationContainer />
       </div>
     );
   }
